90c347f44fce5891720b73b7dd8c4f44f26794d4
maliksaepul <malik@inspigo.id>
2021-03-20 23:55:37 +0700

    first commit belajar mern

---
+++ src/config/routes/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {Login, Home, Register} from '../../pages'
+const Routes = () => {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/login">
+              <Login/>
+          </Route>
+          <Route path="/register">
+              <Register/>
+          </Route>
+          <Route path="/">
+              <Home/>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default Routes;
